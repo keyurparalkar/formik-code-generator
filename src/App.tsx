9d921205c07f32177eec66221d64e37222d88b7b
keyurparalkar <keur.plkar@gmail.com>
2021-11-02 13:14:47 +0530

    feat: added a counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Button, Spinner } from '@blueprintjs/core';
+import Counter from './components/Counter';
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <div className="App">
       <Button title="Hold on" text="Hold On"/>
       <Button icon="refresh" />
-      <Spinner />
+      <Counter />
     </div>
   );
 }
---
+++ src/components/Counter.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button } from "@blueprintjs/core";
+import styled from "styled-components";
+
+const CounterDisplay = styled.span`
+    display: inline-block;
+    padding: 0.5rem 1rem;
+`;
+
+
+const Counter = () => {
+    const [counter, setCounter] = React.useState(0);
+
+    const decrement = () => setCounter((pre: number) => pre - 1);
+    const increment = () => setCounter((pre: number) => pre + 1);
+
+    return (
+        <div>
+        <Button onClick={decrement} icon="minus"/>
+        <CounterDisplay>{counter}</CounterDisplay>
+        <Button onClick={increment} icon="plus"/>
+        </div>
+    );
+}
+
+export default Counter;

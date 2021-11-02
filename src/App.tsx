766fd8c1f62d248b1652be67bdaf935a7a6d1141
keyurparalkar <keur.plkar@gmail.com>
2021-11-02 13:25:40 +0530

    refactor: added package react-syntax-highlighter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { Button, Spinner } from '@blueprintjs/core';
+import { Button } from '@blueprintjs/core';
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import Counter from './components/Counter';
 
 
 function App() {
+  const codeStirng = 'num => num + 1;'
+
   return (
     <div className="App">
       <Button title="Hold on" text="Hold On"/>
       <Button icon="refresh" />
       <Counter />
+      <SyntaxHighlighter language="javascript" style={dark}>
+            {codeStirng}
+        </SyntaxHighlighter>
     </div>
   );
 }

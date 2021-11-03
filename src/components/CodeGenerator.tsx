88d25c47e8d01755bb6bd2c0f1d7210c3008b1e7
keyurparalkar <keur.plkar@gmail.com>
2021-11-03 13:22:28 +0530

    feat: added CodeString utility to return the boilerplate code in syntax highlighter

--- src/components/CodeGenerator.tsx
+++ src/components/CodeGenerator.tsx
@@ -2,24 +2,14 @@
 import { useSelector } from "react-redux";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import CodeString from "./CodeString";
 
 const CodeGenerator = () => {
-  const validationVariablesObj = useSelector((state: any) => state.main.validationVariablesObj);
   const allWidgets = useSelector((state: any) => state.main.allWidgets);
-
-
-  //Create a file with template literal function in it which will take these values.
-  const codeStirng = `
-  const formikValidationObject = Yup.object.shape({
-    ${JSON.stringify(validationVariablesObj)}
-  })
-`;
-
-  console.log("ALL WIDGETS = ", allWidgets);
 
     return (
         <SyntaxHighlighter language="javascript" style={dark}>
-            {codeStirng}
+            {CodeString({allWidgets})}
         </SyntaxHighlighter>
     );
 }

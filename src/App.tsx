03d1f47db49dd1340c3169c00bf23a37313b524b
keyurparalkar <keur.plkar@gmail.com>
2021-11-04 11:10:00 +0530

    style: added flex container, two-sided layout for the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
-import CodeGenerator from './components/CodeGenerator';
-import Counter from './components/Counter';
+import { Alignment, Navbar } from "@blueprintjs/core";
+import CodeGenerator from "./components/CodeGenerator";
+import Counter from "./components/Counter";
+import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 
+  & .left-pan,.right-pan{
+    width: 50%;
+  }
+
+  & .left-pan{
+    height: 100vh;
+  }
+
+  & .right-pan pre{
+    overflow-y: visible;
+    height: 100vh;
+  }
+`;
 
 const App = () => {
   return (
     <div className="formik-code-generator">
-      <div>
-        <label htmlFor="">Text Field</label>
-        <Counter />
-      </div>
-      <CodeGenerator />
+      <Navbar>
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>Formik-Code-Generator</Navbar.Heading>
+          <Navbar.Divider />
+        </Navbar.Group>
+      </Navbar>
+      <Container>
+        <div className="left-pan">
+          <label htmlFor="">Text Field</label>
+          <Counter />
+        </div>
+        <div className="right-pan">
+          <CodeGenerator />
+        </div>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
